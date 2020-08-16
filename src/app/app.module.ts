289a9e849a0031287621ee0261360992afc2fa79
Grasscopper <jssstransky@gmail.com>
2020-08-16 13:50:13 -0400

    learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvengersComponent } from './components/avengers.component'
+import { MegaManComponent } from './components/mega-man.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvengersComponent,
+    MegaManComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/mega-man.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-mega-man',
+  templateUrl: 'mega-man.component.html',
+  styleUrls: ['mega-man.component.css']
+})
+
+export class MegaManComponent {
+
+}
