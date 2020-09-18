9b9d52c14daf3dc19750251da62768563ce58ddb
Grasscopper <jssstransky@gmail.com>
2020-09-18 13:29:00 -0400

    learned how to add a service (not HeroService, but PostService)

--- src/app/components/posts.component.ts
+++ src/app/components/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { Post } from '../models/post.model'
+import { PostService } from '../services/post.service'
 
 @Component({
   selector: "app-posts",
@@ -9,4 +10,8 @@ import { Post } from '../models/post.model'
 
 export class PostsComponent {
   @Input() posts: Post[] = []
+
+  constructor(public postService: PostService) {}
+  //the public keyword is a shortcut for
+  //making postService accessible in this component
 }
